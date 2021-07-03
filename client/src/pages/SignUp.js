a9cc9478f373052f65f4d173c2134bf09dd0a966
pranta-barua007 <prantabarua247@gmail.com>
2021-07-04 01:19:58 +0600

    fully implemented signin feature

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -42,7 +42,7 @@ function SignUp() {
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
                       <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Birthdate <span className="text-red-600">*</span></label>
-                      <input id="email" type="date" className="form-input w-full text-gray-800" required />
+                      <input id="date" type="date" className="form-input w-full text-gray-800" required />
                     </div>
                   </div>
                   <div className="flex flex-wrap -mx-3 mb-4">
