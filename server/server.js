02255451f55c0a34ace9dd57ae6dec18014d7e6a
pranta-barua007 <prantabarua247@gmail.com>
2021-07-04 01:29:44 +0600

    fully implemented signin feature

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
 
+const signin = require('./controllers/signin');
+
 const db = require('knex')({
     client: 'pg',
     connection: {
@@ -14,7 +16,6 @@ const db = require('knex')({
       database : 'evatix'
     }
 });
-
 
 const app = express();
 
@@ -29,6 +30,16 @@ app.get('/*', (req, res) => {
 });
 
 app.get('/check', (req, res) => { res.status(200).send('its working') });
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+
+
+const hash = bcrypt.hashSync('e2pb', 10);
+console.log(hash);
+
+const compare = bcrypt.compareSync('e2pb', '$2a$10$7ksQCb6GhYn4ygPc//o54uyqAH282uKdgjeO73qRhtTjVFpPTRF1y')
+console.log(compare);
+
 
 const PORT = process.env.PORT || 8000;
 
