5850459b39e2e6382bbac9b94d64b96615763198
pranta-barua007 <prantabarua247@gmail.com>
2021-07-05 10:08:30 +0600

    final commit

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -77,7 +77,7 @@ function Profile({ userLoading, currentUser, onSubmitUpdateProfile, onSubmitDele
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-4">
                         <div className="w-full px-3">
-                            <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="profession">Name <span className="text-red-600">*</span></label>
+                            <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="profession">Profession <span className="text-red-600">*</span></label>
                             <input 
                               id="profession" 
                               type="text" 
